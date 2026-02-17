ab5b0ddfc8d2aaad6db99b195a7111a4b98f25a2
getmodpc <getmodpc@gmail.com>
2026-02-18 00:39:34 +0530

    Update com.kmplayer.d

--- com.kmplayer.d
+++ com.kmplayer.d
@@ -1,1 +1,9 @@
-.
+{
+    "Title": "New Update🌿",
+    "versionCode": "0",
+    "versionName": "37.01.281",
+    "AppName": "KMPlayer Plus (Divx) APK",
+    "releaseDate": "",
+    "Msg": "Click the Update button to download the latest version.",
+    "downloadLink": "https://t.me/Getmodpcs/10742"
+}
