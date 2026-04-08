bf5cfbc94c2b16503f5d7164436a34ae92237c7d
getmodpc <getmodpc@gmail.com>
2026-04-08 18:32:25 +0530

    Update and rename 11com.kmplayer.d to com.kmplayer.d

--- com.kmplayer.d
+++ com.kmplayer.d
@@ -1,9 +1,9 @@
 {
     "Title": "New Update🌿",
     "versionCode": "0",
-    "versionName": "37.01.281",
+    "versionName": "37.07.321",
     "AppName": "KMPlayer Plus (Divx) APK",
     "releaseDate": "",
     "Msg": "Click the Update button to download the latest version.",
-    "downloadLink": "https://t.me/Getmodpcs/10742"
+    "downloadLink": "https://t.me/Getmodpcs/11623"
 }
